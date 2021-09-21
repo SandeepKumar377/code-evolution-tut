81a6a65154f989130201df96f3183468d359e83d
Sandeep Kumar <sandeepkumar.sk5.ss55@gmail.com>
2021-09-21 17:21:56 +0530

    use props in class and function base component

--- src/App.js
+++ src/App.js
@@ -6,8 +6,16 @@ import Welcome from './components/Welcome';
 function App() {
   return (
     <div className="App">
-      <Greet />
-      <Welcome />
+      <Greet name="Sandeep" heroName="Salman" >
+        <p>This is children props</p>
+      </Greet>
+      <Greet name="Sendy" heroName="Ajay">
+        <button>Action</button>
+      </Greet >
+      <Greet name="Deep" heroName="Sanjay"/>
+      <Welcome name="Sandeep"  heroName="Salman"/>
+      <Welcome name="Ajay" heroName="Ajay"/>
+      <Welcome name="Sanjay" heroName="Sanjay"/>
       <Hello />
     </div>
   );
