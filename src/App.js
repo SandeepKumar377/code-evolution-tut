d72b4bfbb5148bc4c51f20cc01f59dc6ac6f08c4
Sandeep Kumar <sandeepkumar.sk5.ss55@gmail.com>
2021-09-22 21:07:49 +0530

    Conditional rendring

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import './App.css';
-import ClassClick from './components/ClassClick';
-import Counter from './components/Counter';
-import EventBind from './components/EventBind';
-import FunctionClick from './components/FunctionClick';
-import Greet from './components/Greet';
-import Hello from './components/Hello';
-import Message from './components/Message';
-import ParentComponent from './components/ParentComponent';
-import Welcome from './components/Welcome';
+// import ClassClick from './components/ClassClick';
+// import Counter from './components/Counter';
+// import EventBind from './components/EventBind';
+// import FunctionClick from './components/FunctionClick';
+// import Greet from './components/Greet';
+// import Hello from './components/Hello';
+// import Message from './components/Message';
+// import ParentComponent from './components/ParentComponent';
+import UserGreeting from './components/UserGreeting';
+// import Welcome from './components/Welcome';
 
 function App() {
   return (
     <div className="App">
 
-      <ParentComponent />
+      <UserGreeting />
+      {/* <ParentComponent /> */}
       {/* <FunctionClick /> */}
       {/* <ClassClick /> */}
       {/* <Counter/> */}
---
+++ src/components/UserGreeting.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+
+class UserGreeting extends Component {
+
+    constructor() {
+        super()
+
+        this.state = {
+            isLogedIn: true
+        }
+    }
+
+    render() {
+        //4th short circuit operator
+        //In this approach if condition is true then show data if false then nothing happen 
+        return this.state.isLogedIn && <h1>Hello...Sandeep</h1>
+         // 3rd method using JSX method 
+         return this.state.isLogedIn?<h1>Hello...Sandeep</h1>:<h1>Hello...Guest</h1>
+
+        // 2nd method element varriable approach
+
+        // let message
+        // if (this.state.isLogedIn) {
+        //     message = <h1>Hello...Sandeep</h1>
+        // } else {
+        //     message = <h1>Hello...Guest</h1>
+        // }
+        // return <div>{message}</div>
+
+        // 1st method
+
+        // if (this.state.isLogedIn) {
+        //     return(
+        //         <div>
+        //             <h1>Hello...Sandeep</h1>
+        //         </div>
+        //     )
+        // } else {
+        //     return(
+        //         <div>
+        //             <h1>Hello...Guest</h1>
+        //         </div>
+        //     )            
+        // }
+        // return(
+        //     <div>
+        //         <h1>Hello...Sandeep</h1>
+        //         <h1>Hello...Guest</h1>
+        //     </div>
+        // )
+    }
+}
+
+export default UserGreeting
